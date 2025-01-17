1c3b8145bd02a501a17bc78c07d19e862369a9ce
wayne <profbat018@gmail.com>
2025-01-17 18:49:36 +0400

    Updated

--- Microservices/Part1/CommunicationService/CommunicationService/Contexts/CommunicationContext.cs
+++
@@ -1,8 +0,0 @@
-﻿using Microsoft.EntityFrameworkCore;
-
-namespace CommunicationService.Contexts;
-
-public class CommunicationContext : DbContext
-{
-
-}
--- Microservices/Part1/CommunicationService/CommunicationService/Models/Subscriber.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace CommunicationService.Models
-{
-    public class Subscriber
-    {
-        public Guid Id { get; set; } = Guid.NewGuid();
-        public string OwnerUsername { get; set; } 
-        public string SubscriberUsername { get; set; } 
-    }
-}
